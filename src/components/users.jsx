3656e53eebb478c003a3560d508fd93e899eba01
Alexandr Morgunkov <shadow091@mail.ru>
2022-07-06 18:19:04 +0400

    release task-1

--- src/components/users.jsx
+++ src/components/users.jsx
@@ -1,9 +1,99 @@
-import React from "react";
+import React, {useState} from "react";
 import api from "../api"
 
+
 const Users = () => {
-    console.log(api.users.fetchAll());
-    return <h1>Usres</h1>
-};
+
+    const [users, setUsers] = useState(api.users.fetchAll());
+
+    const qualities = (arrQ, id) => {
+
+        const list = arrQ.map((item, i) => {
+    
+            const classes = `m-2 badge bg-${item.color}`;    
+            return <div key = {id + i} className = {classes}>{item.name}</div>;
+
+        });
+    
+        return list;
+
+    };
+
+    const handlerClick = (id) => {
+
+        setUsers((prevState) => prevState.filter(users => users._id !== id));
+    
+    };
+
+    const userArr = () => {
+        
+        const arr = users.map((user) => {
+            
+                return <tr key = {user._id}>
+                        <td>{user.name}</td>
+                        <td>{qualities(user.qualities, user._id)}</td>
+                        <td>{user.profession.name}</td>
+                        <td>{user.completedMeetings}</td>
+                        <td>{user.rate}/5</td>
+                        <td><button className='btn btn-danger' onClick = {() => handlerClick(user._id)}>delete</button></td>
+                    </tr>;  
+
+            });
+        
+        return arr;
+        }
+        const getBadgeClasses = () => { 
+
+        let classes = "badge ";
+        classes += users.length === 0 ? "bg-danger": "bg-primary";
+
+        return classes;
+
+        }
+        
+        let result = '';
+
+        const textCount = (count) => {
+
+            const lastNumber = count % 10;
+
+            if(lastNumber === 1){
+                return `${count} человек тусанет с тобой сегодня`;
+            }
+            if(((lastNumber === 2 || lastNumber === 3 || lastNumber === 4) && count > 21) || (count === 2 || count === 3 || count === 4) ){
+                return `${count} человека тусанут с тобой сегодня`;
+            }
+            
+            return `${count} человек тусанет с тобой сегодня`;
+
+        }
+
+        if(users.length){
+         
+         result = <>
+           <span className={getBadgeClasses()}>{textCount(users.length)}</span>
+           <table className="table">            
+                    <thead>
+                        <tr>
+                            <th scope="col">Имя</th>
+                            <th scope="col">Качества</th>
+                            <th scope="col">Профессия</th>
+                            <th scope="col">Встретился, раз</th>
+                            <th scope="col">Оценка</th>
+                            <th scope="col">Ubn</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {userArr()}                
+                    </tbody>
+                </table>
+                </>
+        } else{
+            result =  <span className={getBadgeClasses()}>Никто не хочет с тобой тусить</span>;
+        };
+
+           return result;
+           
+    };
 
 export default Users;
