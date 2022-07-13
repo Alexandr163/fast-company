f38d487e22284c6021f4a20c49f88d59a660bcc7
Alexandr Morgunkov <shadow091@mail.ru>
2022-07-13 22:56:20 +0400

    add nwe components

---
+++ src/app.jsx
@@ -0,0 +1,66 @@
+import React, { useState } from "react";
+import Users from "./components/users";
+import SearchStatus from "./components/searchStatus";
+import api from "./api"
+
+const App = () => {
+    const [users, setUsers] = useState(api.users.fetchAll());
+
+    const handleDelete = (userId) =>
+      setUsers(users.filter((user) => user._id !== userId));
+      return (
+        <>
+          <h2>
+            <span
+              className={"badge  bg-" + (users.length > 0 ? "primary" : "danger")}
+            >
+              {users.length > 0
+                ? `${users.length} ${renderPhrase(users.length)} с тобой сегодня`
+                : "Никто с тобой не тусанет"}
+            </span>
+          </h2>
+          {users.length > 0 && (
+            <table className="table">
+              <thead>
+                <tr>
+                  <th scope="col">Имя</th>
+                  <th scope="col">Качества</th>
+                  <th scope="col">Профессия</th>
+                  <th scope="col">Встретился, раз</th>
+                  <th scope="col">Оценка</th>
+                  <th />
+                </tr>
+              </thead>
+              <tbody>
+                {users.map((user) => (
+                  <tr key={user._id}>
+                    <td>{user.name}</td>
+                    <td>
+                      {user.qualities.map((item) => (
+                        <span
+                          className={"badge m-1 bg-" + item.color}
+                          key={item._id}
+                        >
+                          {item.name}
+                        </span>
+                      ))}
+                    </td>
+                    <td>{user.profession.name}</td>
+                    <td>{user.completedMeetings}</td>
+                    <td>{user.rate}</td>
+                    <td>
+                      <button
+                        className={"btn btn-danger"}
+                        onClick={() => handleDelete(user._id)}
+                      >
+                        Удалить
+                      </button>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          )}
+        </>
+      );
+    };   
---
+++ src/components/user.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import Qualitie from "./qualitie";
+import Bookmark from "./bookmark";
+const User = () => {
+
+};
+
+export default User;
