8897e2b39c849755bdae1a5ef9f3f93732312ab4
Alexandr Morgunkov <shadow091@mail.ru>
2022-07-25 23:52:55 +0400

    finished learn_6

--- src/components/users.jsx
+++ src/components/users.jsx
@@ -1,20 +1,35 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { paginate } from "../utils/paginate";
 import Pagination from "./pagination";
 import User from "./user";
+import api from "../api";
 import PropTypes from "prop-types";
+import GroupList from "./groupList";
 
 const Users = ({ users, ...rest }) => {
+    const [currentPage, setCurrentpage] = useState(1);
+    const [professions, setProfessions] = useState();
     const count = users.length;
     const pageSize = 4;
-    const [currentPage, setCurrentpage] = useState(1);
+    useEffect(() => {
+        api.professions.fetchAll().then((data) => setProfessions(data));
+    }, []);
+
+    const handleProfessionSelect = (params) => {
+        console.log(params);
+    };
     const handlePageChange = (pageIndex) => {
         setCurrentpage(pageIndex);
     };
-
     const userCrop = paginate(users, currentPage, pageSize);
     return (
         <>
+            {professions && (
+                <GroupList
+                    items={professions}
+                    onItemselect={handleProfessionSelect}
+                />
+            )}
             {count > 0 && (
                 <table className="table">
                     <thead>
