38ca4df4adb39b7daf4b10e29f567fad3bc0204b
Alexandr Morgunkov <shadow091@mail.ru>
2022-08-17 20:18:03 +0400

    finished my homework

--- src/components/userPage.jsx
+++ src/components/userPage.jsx
@@ -1,7 +1,41 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import PropTypes from "prop-types";
+import api from "../api";
+import QualitiesList from "./qualitiesList";
+import { useHistory } from "react-router-dom";
 
-const UserPage = () => {
-    return <h1>UserPage</h1>;
+const UserPage = ({ id }) => {
+    const history = useHistory();
+    const [user, setUser] = useState();
+    useEffect(() => {
+        api.users.getById(id).then((data) => setUser(data));
+    }, []);
+
+    let renderUserPage = null;
+    const handleReturne = () => {
+        history.replace("/users");
+    };
+    if (user) {
+        renderUserPage = (
+            <>
+                <h1>{user.name}</h1>
+                <h2>{`Професиия: ${user.profession.name}`}</h2>
+                <QualitiesList qualities={user.qualities} />
+                <h5>{`CompletedMeetings: ${user.completedMeetings}`}</h5>
+                <h1>{`Rate: ${user.rate}`}</h1>
+
+                <button type="button" className="btn btn-outline-secondary" onClick={handleReturne}>Все пользователи</button>
+            </>
+        );
+    } else {
+        renderUserPage = "loading...";
+    }
+
+    return renderUserPage;
+};
+
+UserPage.propTypes = {
+    id: PropTypes.string
 };
 
 export default UserPage;
--- src/components/usersList.jsx
+++ src/components/usersList.jsx
@@ -1,18 +1,12 @@
-import { React, useEffect, useState } from "react";
+import { React } from "react";
 import { useParams } from "react-router-dom";
 import UserPage from "./userPage";
 import Users from "./users";
-import api from "../api";
 
 const UsersList = () => {
     const { usersId } = useParams();
 
-    const [user, setUser] = useState();
-    useEffect(() => {
-        api.users.getById(user).then((data) => setUser(data));
-    }, []);
-
-    return usersId ? <UserPage /> : <Users />;
+    return usersId ? <UserPage id={usersId} /> : <Users />;
 };
 
 export default UsersList;
