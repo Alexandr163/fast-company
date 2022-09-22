57a45850a736884fab8fe86b371ea3a6601d64f1
Alexandr Morgunkov <shadow091@mail.ru>
2022-09-22 09:56:44 +0400

    finished homework

---
+++ src/app/components/page/userPage/displayComments/comments.jsx
@@ -0,0 +1,54 @@
+import { orderBy } from "lodash";
+import React, { useState, useEffect } from "react";
+import api from "../../../../api";
+import { useParams } from "react-router-dom";
+import AddComment from "./addComment";
+import CommentList from "./commentList";
+
+const Comments = () => {
+    const { userId } = useParams();
+    const [comments, setComments] = useState([]);
+    useEffect(() => {
+        api.comments
+            .fetchCommentsForUser(userId)
+            .then((data) => setComments(data));
+    }, []);
+    const handleSubmit = (data) => {
+        api.comments
+            .add({ ...data, pageId: userId })
+            .then((data) => setComments([...comments, data]));
+    };
+    const handleRemoveComment = (id) => {
+        api.comments.remove(id).then((id) => {
+            setComments(comments.filter((x) => x._id !== id));
+        });
+    };
+    const sortedComments = orderBy(comments, ["created_at"], ["dasc"]);
+    console.log(sortedComments);
+    return (
+        <>
+            <div className="col-md-8">
+                <div className="card mb-2">
+                    {" "}
+                    <div className="card-body ">
+                        <AddComment onSubmit={handleSubmit} />
+                    </div>
+                </div>
+                {sortedComments.length > 0 && (
+                    <div className="card mb-3">
+                        <div className="card-body ">
+                            <h2>Comments</h2>
+                            <hr />
+                            <CommentList
+                                comments={sortedComments}
+                                onRemove={handleRemoveComment}
+                            />
+                        </div>
+                    </div>
+                )}
+            </div>
+        </>
+    );
+};
+
+export default Comments;
--- src/app/components/page/userPage/userPage.jsx
+++ src/app/components/page/userPage/userPage.jsx
@@ -1,34 +1,23 @@
 import React, { useEffect, useState } from "react";
 import PropTypes from "prop-types";
 import api from "../../../api";
-import Qualities from "../../ui/qualities";
-import { useHistory } from "react-router-dom";
+import UserInfo from "./userInfo/userInfo";
+import Comments from "./displayComments/comments";
 
 const UserPage = ({ userId }) => {
-    const history = useHistory();
     const [user, setUser] = useState();
     useEffect(() => {
         api.users.getById(userId).then((data) => setUser(data));
     }, []);
-    const handleClick = () => {
-        history.push(`/users/${user._id}/edit`);
-    };
     if (user) {
         return (
             <div>
-                <h1> {user.name}</h1>
-                <h2>Профессия: {user.profession.name}</h2>
-                <Qualities qualities={user.qualities} />
-                <p>completedMeetings: {user.completedMeetings}</p>
-                <h2>Rate: {user.rate}</h2>
-                <button
-                    type="button"
-                    className="btn btn-secondary"
-                    onClick={handleClick}
-                >
-                    {" "}
-                    Изменить
-                </button>
+                <div className="container">
+                    <div className="row gutters-sm">
+                        <UserInfo user={user} />
+                        <Comments />
+                    </div>
+                </div>
             </div>
         );
     } else {
