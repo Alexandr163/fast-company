c17b0eabf9805c4b73be4ce4893346db7276a5ad
Alexandr Morgunkov <shadow091@mail.ru>
2022-09-25 17:06:40 +0400

    commit

--- src/app/components/page/userPage/userPage.jsx
+++ src/app/components/page/userPage/userPage.jsx
@@ -1,8 +1,10 @@
 import React, { useEffect, useState } from "react";
 import PropTypes from "prop-types";
 import api from "../../../api";
-import UserInfo from "./userInfo/userInfo";
-import Comments from "./displayComments/comments";
+import UserCard from "../../ui/userCard";
+import QualitiesCard from "../../ui/qualitiesCard";
+import MeetingsCard from "../../ui/meetingsCard";
+import Comments from "../../ui/comments";
 
 const UserPage = ({ userId }) => {
     const [user, setUser] = useState();
@@ -11,10 +13,14 @@ const UserPage = ({ userId }) => {
     }, []);
     if (user) {
         return (
-            <div>
-                <div className="container">
-                    <div className="row gutters-sm">
-                        <UserInfo user={user} />
+            <div className="container">
+                <div className="row gutters-sm">
+                    <div className="col-md-4 mb-3">
+                        <UserCard user={user} />
+                        <QualitiesCard data={user.qualities} />
+                        <MeetingsCard value={user.completedMeetings} />
+                    </div>
+                    <div className="col-md-8">
                         <Comments />
                     </div>
                 </div>
